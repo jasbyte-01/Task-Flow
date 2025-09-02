27d6aa84dbdaf21305a086dff03ee71a383b04ba
waute <waute@JASOS>
2025-09-02 21:22:23 +0200

    Add database as code first

--- Infrastructure/Data/TaskFlowDbContext.cs
+++ Infrastructure/Data/TaskFlowDbContext.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 public partial class TaskFlowDbContext : DbContext
 {
-    public TaskFlowDbContext() { }
-
     public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options)
         : base(options) { }
 
+    public virtual DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07641F45F4");
+
+            entity.ToTable("Users", "core");
+
+            entity.HasIndex(e => e.UserName, "UQ__Users__C9F28456A976EFFE").IsUnique();
+
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreationDate).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.UserName).HasMaxLength(100);
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
---
+++ Infrastructure/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Models;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public DateTime CreationDate { get; set; }
+
+    public DateTime? LastChangedDate { get; set; }
+}
