063c59b5b7a477bc3d7c3307b13da79e7dbf4920
waute <waute@JASOS>
2025-09-06 16:33:20 +0200

    Fix migration issues

--- Infrastructure/Data/TaskFlowDbContext.cs
+++ Infrastructure/Data/TaskFlowDbContext.cs
@@ -9,24 +9,4 @@ public partial class TaskFlowDbContext : DbContext
         : base(options) { }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07641F45F4");
-
-            entity.ToTable("Users", "core");
-
-            entity.HasIndex(e => e.UserName, "UQ__Users__C9F28456A976EFFE").IsUnique();
-
-            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
-            entity.Property(e => e.CreationDate).HasDefaultValueSql("(getdate())");
-            entity.Property(e => e.UserName).HasMaxLength(100);
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ Infrastructure/Data/TaskFlowDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Data
+{
+    public class TaskFlowDbContextFactory : IDesignTimeDbContextFactory<TaskFlowDbContext>
+    {
+        public TaskFlowDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<TaskFlowDbContext> optionsBuilder = new();
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=TaskFlow;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+
+            return new TaskFlowDbContext(optionsBuilder.Options);
+        }
+    }
+}
